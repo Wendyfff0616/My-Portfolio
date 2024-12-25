78ad2c08f4f166484434b29a2d628571ec037ca6
Wenyu Fan <wfan0971@uni.sydney.edu.au>
2024-12-25 15:18:23 +0800

    Footer finished

---
+++ src/components/Footer.js
@@ -0,0 +1,25 @@
+import { Container, Row, Col } from 'react-bootstrap';
+import logo from "../assets/img/logo.svg";
+import navIcon1 from '../assets/img/nav-icon1.svg';
+
+export const Footer = () => {
+    return (
+        <footer className="footer">
+            <Container>
+                <Row className="align-item-center">
+                    <Col sm={6}>
+                        <img src={logo} alt="logo"/>
+                    </Col>
+                    <Col sm={6} className='text-center text-sm-end'>
+                        <div className='social-icon'>
+                            <a href="#linkedin"><img src={navIcon1} alt="LinkedIn"/></a>
+                            <a href="#behance"><img src={navIcon1} alt="Behance"/></a>
+                            <a href="#email"><img src={navIcon1} alt="Email"/></a>
+                        </div>
+                        <p>CopyRight 2024. All Right Reserved </p>
+                    </Col>
+                </Row>
+            </Container>
+        </footer>
+    )
+}
